eded61ca64bee06d77d5f47037cba50f3673a959
Kunal Bhati <kbnikku@gmail.com>
2025-11-03 23:50:35 +0530

    Banner Updated

--- src/pages/home page/components/Banner.tsx
+++ src/pages/home page/components/Banner.tsx
@@ -1,7 +1,7 @@
 function Banner() {
     return (<>
-        <section className="w-[80%] mx-auto py-8 md:grid grid-cols-2">
-            <div>
+        <section className=" md:grid grid-cols-2 bg-[url(/public/images/home_banner_image_1.jpg)] bg-cover">
+            <div className="w-[80%] mx-auto py-8">
                 <h1 className="text-2xl font-semibold sm:font-bold">We Provide <span className="text-primary">Medical</span> Services That You Can <span className="text-primary">Trust!</span></h1>
                 <p className="py-3 text-sm">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi ipsam quidem aperiam libero, incidunt numquam.</p>
                 <div className="flex flex-col gap-2 text-white font-medium sm:flex-row">
@@ -9,7 +9,7 @@ function Banner() {
                     <button className="bg-primary-dark hover:bg-primary px-4 py-2 rounded-sm w-full">Learn More</button>
                 </div>
             </div>
-            <div>
+            <div className="hidden">
                 <p>hello</p>
             </div>
         </section>
